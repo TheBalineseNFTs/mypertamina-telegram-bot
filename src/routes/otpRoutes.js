7a3e83b623f6e5a20e5fcc24773ba0790393ad71
AKOKs <109363606+TheBalineseNFTs@users.noreply.github.com>
2024-11-01 09:53:40 +0700

    Create otpRoutes.js

---
+++ src/routes/otpRoutes.js
@@ -0,0 +1,9 @@
+// src/routes/otpRoutes.js
+const express = require('express');
+const router = express.Router();
+const otpController = require('../controllers/otpController');
+
+router.post('/request', otpController.requestOtp);
+router.post('/verify', otpController.verifyOtp);
+
+module.exports = router;
