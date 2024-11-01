aca1ff0efa69acee51c35493976f3d8fd4ca7502
AKOKs <109363606+TheBalineseNFTs@users.noreply.github.com>
2024-11-01 10:48:19 +0700

    Update sessionStore.js

--- src/utils/sessionStore.js
+++ src/utils/sessionStore.js
@@ -8,3 +8,39 @@ exports.setOtp = (otp) => {
 exports.getOtp = () => {
   return otpCode;
 };
+const sessions = {};
+
+// Menambahkan sesi pengguna baru
+function addSession(chatId, userInfo) {
+    sessions[chatId] = userInfo;
+}
+
+// Mendapatkan informasi sesi pengguna berdasarkan chatId
+function getSession(chatId) {
+    return sessions[chatId];
+}
+
+// Menghapus sesi pengguna berdasarkan chatId
+function removeSession(chatId) {
+    delete sessions[chatId];
+}
+
+// Mengupdate sesi pengguna
+function updateSession(chatId, newUserInfo) {
+    if (sessions[chatId]) {
+        sessions[chatId] = { ...sessions[chatId], ...newUserInfo };
+    }
+}
+
+// Mendapatkan semua sesi pengguna (jika diperlukan)
+function getAllSessions() {
+    return sessions;
+}
+
+module.exports = {
+    addSession,
+    getSession,
+    removeSession,
+    updateSession,
+    getAllSessions,
+};
