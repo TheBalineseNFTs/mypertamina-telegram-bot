5d0c12658cc4b065973a9bc2a8305b028d6b1554
AKOKs <109363606+TheBalineseNFTs@users.noreply.github.com>
2024-11-01 11:02:55 +0700

    Update otpService.js

--- src/services/otpService.js
+++ src/services/otpService.js
@@ -1,12 +1,6 @@
-// src/services/otpService.js
-const sessionStore = require('../utils/sessionStore');
-
-exports.generateOtp = () => {
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
-  sessionStore.setOtp(otp);
-  return otp;
+// Contains functions to generate and validate OTPs
+const generateOTP = () => {
+    return Math.floor(10000 + Math.random() * 90000).toString(); // 6-digit OTP
 };
 
-exports.verifyOtp = (otp) => {
-  return sessionStore.getOtp() === otp;
-};
+module.exports = { generateOTP };
