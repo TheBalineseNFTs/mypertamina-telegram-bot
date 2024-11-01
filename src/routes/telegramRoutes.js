4cbfcb883e79d58e7cef83edbd6bdee09905e117
AKOKs <109363606+TheBalineseNFTs@users.noreply.github.com>
2024-11-01 11:03:29 +0700

    Update telegramRoutes.js

--- src/routes/telegramRoutes.js
+++ src/routes/telegramRoutes.js
@@ -1,8 +1,9 @@
-// src/routes/telegramRoutes.js
 const express = require('express');
 const router = express.Router();
 const telegramController = require('../controllers/telegramController');
 
-router.post('/sendOtp', telegramController.sendOtpMessage);
+router.post('/request_telegram_code', telegramController.requestTelegramCode);
+
+// Implement other routes related to Telegram commands...
 
 module.exports = router;
